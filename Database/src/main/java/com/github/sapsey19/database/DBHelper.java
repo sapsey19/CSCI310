38f42e0db2d9c9b595fced3f8e18ba215a310746
Sean Apsey <ssaspey@mavs.coloradomesa.edu>
2018-10-25 20:22:52 -0600

    Updated project

---
+++ Database/src/main/java/com/github/sapsey19/database/DBHelper.java
@@ -0,0 +1,93 @@
+package com.github.sapsey19.database;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Sean Apsey
+ */
+public class DBHelper {
+
+    public static String DEFAULT_DB = "tests.db";
+    public static String DEFAULT_URL = "jdbc:sqlite:" + DEFAULT_DB;
+
+    public static void createNewDatabase(String fileName) {
+
+        try (Connection conn = DriverManager.getConnection(DEFAULT_URL)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void createNewTable() {
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(DEFAULT_URL);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private Connection connect() {
+
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DEFAULT_URL);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void selectAll() {
+        String sql = "SELECT id, name, capacity FROM warehouses";
+
+        try (Connection conn = this.connect();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t"
+                        + rs.getString("name") + "\t"
+                        + rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
+
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
--- Database/src/main/java/com/github/sapsey19/database/Main.java
+++ Database/src/main/java/com/github/sapsey19/database/Main.java
@@ -1,41 +1,23 @@
 package com.github.sapsey19.database;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
- 
 /**
  *
  * @author Saspey
  */
 public class Main {
- 
-    /**
-     * Connect to a sample database
-     *
-     * @param fileName the database file name
-     */
-    public static void createNewDatabase(String fileName) {
- 
-        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
- 
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-            }
- 
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
- 
-    /**
-     * @param args the command line arguments
-     */
+
+    public static String DEFAULT_DB = "tests.db";
+    public static String DEFAULT_URL = "jdbc:sqlite:" + DEFAULT_DB;
+
     public static void main(String[] args) {
-        createNewDatabase("test.db");
+
+        DBHelper db = new DBHelper();
+
+        //db.createNewDatabase(DEFAULT_DB);
+        //db.createNewTable();
+        db.insert("Raw Materials", 3000);
+        db.insert("Semifinished Goods", 4000);
+        db.insert("Finished Goods", 5000);
+        db.selectAll();
     }
 }
